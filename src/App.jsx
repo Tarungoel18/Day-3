f051fd8249cd6126037dd5cf373ea5d3e6a594b8
Tarun Goel <tarungoel1823@gmail.com>
2026-01-30 19:56:50 +0530

    Day_03_01_03

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import User from "./components/user/Index.jsx";
 function App() {
   return (
-    <React.Fragment>
+    <>
       <User />
-    </React.Fragment>
+    </>
   );
 }
 
