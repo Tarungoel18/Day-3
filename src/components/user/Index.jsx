de5b9de57e5eb9051069132f8202549e7be5f469
Tarun Goel <tarungoel1823@gmail.com>
2026-01-30 20:15:21 +0530

    Day_03_01_04 and Day_03_01_05

--- src/components/user/Index.jsx
+++ src/components/user/Index.jsx
@@ -1,6 +1,37 @@
+import "./user.css"
+import { useRef } from "react"
 const Index = () => {
+  const inputEmail = useRef()
+  const inputPassword = useRef()
+
+    const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Email:", inputEmail.current.value);
+    console.log("Password:", inputPassword.current.value);
+    inputEmail.current.value = "";
+    inputPassword.current.value = "";
+  };
+
+  
   return (
-    <div>User</div>
+    <div className="parent-container">
+    <div className="main-container">
+      <div className="form-element">
+        <label>Email</label>
+        <input ref={inputEmail} type="text"/>     
+      </div>
+      <div className="form-element">
+       <label>Password</label>
+      <input ref={inputPassword} type="text"/>
+      </div>
+        <div className="form-element">
+          <button className="submit-btn" onClick={handleSubmit}>
+            Submit
+          </button>
+        </div>
+      </div>
+     
+    </div>
   )
 }
 
